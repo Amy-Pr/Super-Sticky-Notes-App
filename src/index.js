5a61200008e24a6aae52becee41f5b5e048feadd
amy_pr <ampreci@gmail.com>
2024-07-31 22:29:24 -0700

    update root file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
