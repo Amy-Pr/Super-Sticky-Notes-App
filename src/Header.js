e083ba1a2f40a8c40ab01b218a0b5d9b7109ef4f
amy_pr <ampreci@gmail.com>
2024-07-31 23:42:49 -0700

    destructure props in Header

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Header = (props) => (
+const Header = ({addNoteProps, searchTextProps, onSearchProps}) => (
   <header>
     <h1 className="app-header__title">Super Sticky Notes</h1>
     <aside className="app-header__controls">
-      <button className="add-new" onClick={props.addNoteProps}>
+      <button className="add-new" onClick={addNoteProps}>
         + New Note
       </button>
       <input
         type=""
         className="search"
         placeholder="Type here to search..."
-        value={props.searchTextProps}
-        onChange={props.onSearchProps}
+        value={searchTextProps}
+        onChange={onSearchProps}
       />
     </aside>
   </header>
